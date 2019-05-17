5262ecfd2f1c54152225c4494faa934fc88a4702
UsamaAslam <uaslam345@gmail.com>
2019-05-17 17:36:11 +0500

    patch /task/:id is setup

--- server/routes/TaskRoute.js
+++ server/routes/TaskRoute.js
@@ -51,4 +51,40 @@ router.get("/task/:id", async (req, res) => {
   }
 });
 
+router.patch("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  const fields = ["text", "completed"];
+
+  if (!ObjectID.isValid(id) || !ValidField(req, fields))
+    return res.status(400).send({ error: "invalid id or input" });
+
+  const body = _.pick(req.body, fields);
+  try {
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+      const task = await Task.findByIdAndUpdate(
+        id,
+        { $set: body },
+        { new: true }
+      );
+
+      if (!task) return res.status(400).send({ error: "task not found" });
+
+      res.status(200).send(task);
+    } else if (_.isBoolean(body.completed) && !body.completed) {
+      body.completed = false;
+      body.completedAt = null;
+      const task = await Task.findByIdAndUpdate(
+        id,
+        { $set: body },
+        { new: true }
+      );
+      if (!task) return res.status(400).send({ error: "task not found" });
+      res.status(200).send({ task });
+    } else res.status(400).send({ error: "invalid data" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
